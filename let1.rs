5fb429fa41149ea4b269541dfbc4adbe0eb73a06
akxl <aaronleong32@gmail.com>
2018-12-29 14:07:47 +0000

    rust simple stuff

---
+++ let1.rs
@@ -0,0 +1,7 @@
+// let1.rs
+// learning how to declare a variable
+
+fn main() {
+    let answer = 42;
+    println!("Hello {}", answer);
+}
---
+++ let2.rs
@@ -0,0 +1,7 @@
+// let2.rc
+// learning about the assert_eq!() macro
+
+fn main() {
+    let answer = 40;
+    assert_eq!(answer, 42); // let's get the programme to panic
+}
