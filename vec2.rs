1c19b2632a8ebf25566530b3d897b0a583751654
akxl <aaronleong32@gmail.com>
2018-12-30 14:21:15 +0000

    vectors

---
+++ vec2.rs
@@ -0,0 +1,18 @@
+// vec2.rs
+// relationship between vectors and slices
+
+fn dump(arr: &[i32]) {
+    println!("arr is {:?}", arr);
+}
+
+fn main() {
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(20);
+    v.push(30);
+
+    dump(&v);
+
+    let slice = &v[1..];
+    println!("slice is {:?}", slice);
+}
