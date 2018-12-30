e092ecbe3107d8cbee16e44a7804e1146783a022
akxl <aaronleong32@gmail.com>
2018-12-30 12:52:49 +0000

    arrays

---
+++ cos.rs
@@ -0,0 +1,10 @@
+// cos.rs
+// using the cosing function and importing constants
+
+use std::f64::consts;
+
+fn main() {
+    let x = 2.0 * consts::PI;
+    let abs_difference = (x.cos() - 1.0).abs();
+    assert!(abs_difference < 1e-10);
+}
