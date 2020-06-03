a9ef09605775bfc815deaf9b7c82c0f7c4b2d73d
tworm <tworm@protonmail.com>
2020-06-03 21:37:49 +0100

    extracting common code

--- other_code/solve_me_first.rs
+++ other_code/solve_me_first.rs
@@ -1,30 +1,30 @@
 use std::io;
 
+fn parse_string_to_number(input: String) -> i32 {
+    return input.trim()
+            .parse::<i32>()
+            .ok()
+            .expect("parse error");
+}
+
+fn read_string_from_stdin() -> String {
+    let mut the_string = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut the_string)
+        .ok()
+        .expect("read error");
+    return the_string;
+}
+
 fn main() {
 
-    let mut _number_string_1 = String::new();
-    let mut _number_string_2 = String::new();
-    let stdin = io::stdin();
-
-    stdin.read_line(&mut _number_string_1)
-        .ok()
-        .expect("read error");
+    let _number_string_1 = read_string_from_stdin();
+    let _number_string_2 = read_string_from_stdin();
     
-    stdin.read_line(&mut _number_string_2)
-        .ok()
-        .expect("read error");
-    
-    let _number_1 = _number_string_1
-                    .trim()
-                    .parse::<i32>()
-                    .ok()
-                    .expect("parse error");
-    let _number_2 = _number_string_2
-                    .trim()
-                    .parse::<i32>()
-                    .ok()
-                    .expect("parse error");
+    let _number_1 = parse_string_to_number(_number_string_1);
+    let _number_2 = parse_string_to_number(_number_string_2);
     
     println!("{}", _number_1 + _number_2);
+
 
 }
